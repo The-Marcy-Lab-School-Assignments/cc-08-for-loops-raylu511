50dc413c8c8182c84071414a1c3b3c4b522c8a81
Ray Lu <raylu511@gmail.com>
2021-10-26 13:54:53 +0000

    Finished code challenge

--- index.js
+++ index.js
@@ -1,25 +1,45 @@
 //1
 function countToTwenty(){
-  
+  for(let i=1; i<=20; i++){
+      console.log(i);
+  }
 }
 
 //2
 function countFromTwenty(){
-  
+  for(let i=20;i>=1;i--){
+      console.log(i);
+  }
 }
 
 //3
-function countToN(){
-  
+function countToN(n){
+  for(let i=1; i<=n;i++){
+      console.log(i);
+  }
 }
 
 
 //4
-function countFromN(){
-  
+function countFromN(n){
+  for(let i=n;i>=1;i--){
+      console.log(i);
+  }
 }
 
 //5
-function countEveryOdd(){
-  
+function countEveryOdd(int){
+  for(let i=1;i<=int;i++){
+      if(i%2===1){
+          console.log(i);
+      }
+  }
 }
+
+
+
+countToTwenty();                    //1-20
+countFromTwenty();                  //20-1
+countToN(10);                       //1-10
+countFromN(10);                     //10-1
+countEveryOdd(10);                  //1 3 5 7 9
